0baf61c5782ee508f81b5598a0850a391dd30cea
luyuhao <ludaye1112@163.com>
2020-12-03 01:53:53 +0800

    描述：初始化部分结构 日期：2020-12-03

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,64 @@
 <template>
-  <div>
-    <p>hello world</p>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+
+      <div class="title-container">
+        <h3 class="title">注册登录DPTMS</h3>
+      </div>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="用户名"
+          name="username"
+          type="text"
+          tabindex="1"
+          auto-complete="on"
+        />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :key="passwordType"
+          ref="password"
+          v-model="loginForm.password"
+          :type="passwordType"
+          placeholder="密码"
+          name="password"
+          tabindex="2"
+          auto-complete="on"
+          @keyup.enter.native="handleLogin"
+        />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+
+      <el-form-item prop="code" class="code-input">
+        <span class="svg-container">
+          <svg-icon icon-class="verify-code" />
+        </span>
+        <el-input
+          v-model="loginForm.code"
+          placeholder="验证码"
+          name="code"
+          type="text"
+          tabindex="3"
+          @keyup.enter.native="handleLogin"
+        />
+      </el-form-item>
+      <!--<img :src="code.img" alt="" class="code-image" @click="getcode">-->
+
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登 录</el-button>
+
+    </el-form>
   </div>
 </template>
 
@@ -10,7 +68,19 @@ export default {
   name: 'Login',
   components: {},
   data() {
-    return {}
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+        loginType: 1
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', message: '请输入用户名' }],
+        password: [{ required: true, trigger: 'blur', message: '请输入密码' }]
+      },
+      passwordType: 'password',
+      loading: false
+    }
   },
   watch: {},
   created() {
@@ -18,10 +88,31 @@ export default {
   },
   mounted() {
   },
-  methods: {}
+  methods: {
+    showPwd() {
+      if (this.passwordType === 'password') {
+        this.passwordType = ''
+      } else {
+        this.passwordType = 'password'
+      }
+      this.$nextTick(() => {
+        this.$refs.password.focus()
+      })
+    },
+    handleLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          console.log("login");
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-
+  @import "login";
+</style>
+<style lang="scss" scoped>
+  @import "login-scoped";
 </style>
