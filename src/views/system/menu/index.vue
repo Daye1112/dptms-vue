a00bb6408f6e81b7ee998e0f4a689f30723e67ff
luyuhao <ludaye1112@163.com>
2021-01-04 00:59:32 +0800

    描述：开发菜单管理页面 日期：2021-01-04

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,15 +1,77 @@
 <template>
   <div class="app-container">
-    菜单管理
+    <el-row class="menu_container">
+      <el-col :span="4" class="menu_tree">
+        <el-scrollbar wrap-class="scrollbar-wrapper">
+          <el-tree
+            :data="menuTree"
+            node-key="id"
+            @node-click="listPermission"
+            :props="defaultProps">
+          </el-tree>
+        </el-scrollbar>
+      </el-col>
+      <el-col :span="20">
+
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
+import Pagination from '@/components/Pagination'
+import elDragDialog from '@/directive/el-drag-dialog'
+import waves from '@/directive/waves'
+
 export default {
-  name: "Menu"
+  name: "Menu",
+  components: {Pagination},
+  directives: {waves, elDragDialog},
+  data() {
+    return {
+      menuTree: [],
+      defaultProps: {
+        children: 'children',
+        label: 'menuName'
+      }
+    }
+  },
+  created() {
+    this.listTree();
+  },
+  methods: {
+    listTree() {
+      request.get("/system-manage/sys/menu/listTree", {})
+        .then(response => {
+          let data = response.data;
+          if (data) {
+            this.menuTree.push(data);
+          }
+        });
+    },
+    listPermission(node) {
+      console.log(node);
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .app-container {
+    height: 100%;
 
+    .menu_container {
+      height: 100%;
+
+      .menu_tree {
+        height: 100%;
+        border-right: 1px solid #e3e3e3;
+
+        .el-scrollbar {
+          height: 100%;
+        }
+      }
+    }
+  }
 </style>
